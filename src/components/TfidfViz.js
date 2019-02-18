89beffd862d7fb3a4da8f56267ab9218ee9c5864
Rob Carney <robert.edward.carney@gmail.com>
2019-02-18 17:19:12 -0500

    Adding base component for the app

---
+++ src/components/TfidfViz.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+
+class TfidfViz extends Component  {
+    constructor(props)  {
+        super(props);
+    }
+
+    render()  {
+        return (
+            <h1>Tfidf.Viz</h1>
+        );
+    }
+
+}
+
+export default TfidfViz;
+
+
