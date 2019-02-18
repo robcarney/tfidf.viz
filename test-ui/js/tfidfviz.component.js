bf82a9183e845d90b60383174a5bb35f5f1ed209
Rob Carney <robert.edward.carney@gmail.com>
2019-02-18 17:06:29 -0500

    More UI work

--- test-ui/js/tfidfviz.component.js
+++ test-ui/js/tfidfviz.component.js
@@ -1,12 +1,27 @@
 (function()  {
+    var $currId;
+
     $(main);
 
     function main()  {
+        $currId = 2;
+
         $(".tv-doc-title-input").change(function()  {
             var newTitle = $(this).val();
             var id = $(this).parent().parent().attr("id");
             var idClass = "." + id;
             $(idClass).find(".tab-link").text(newTitle);
         });
+
+        markParagraph();
+    }
+
+    function markParagraph()  {
+        var paragraphText = $(".tv-doc-result-text").text();
+        paragraphText = paragraphText.replace(/[,\.]/, "");
+        console.log(paragraphText);
+        paragraphText = paragraphText.replace(/\s{2,}/," ");
+        var words = paragraphText.split(" ");
+        console.log(words);
     }
 })();
