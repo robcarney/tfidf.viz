8188ee27747e35dd48d2976cefa2dd1ef172e957
Rob Carney <robert.edward.carney@gmail.com>
2019-02-24 16:04:18 -0500

    Adding valueNormalized field to word scores for coloring

--- src/services/TfidfService.js
+++ src/services/TfidfService.js
@@ -18,8 +18,10 @@ export default class TfidfService  {
 
     responseToDocs(responseJson)  {
         let docs = [];
+        // Create result docs array
         for (let i = 0; i < responseJson.docs.length; i++)  {
             let currWordScores = [];
+            // WordToScore comes in with words as props, convert this to an array of wordToScore objects
             for (let prop in responseJson.docs[i].wordToScore)  {
                 if (responseJson.docs[i].wordToScore.hasOwnProperty(prop))  {
                     if (responseJson.docs[i].wordToScore[prop] !== 0) {
@@ -35,6 +37,23 @@ export default class TfidfService  {
                 wordScores: currWordScores
             })
         }
+        // Normalize word score values (set to values b/w 0 and 1)
+        let max = 0.0;
+        for (let i = 0; i < docs.length; i++)  {
+            for (let j = 0; j < docs[i].wordScores.length; j++)  {
+                let currWordScore = docs[i].wordScores[j]
+                if (currWordScore.value > max)  {
+                    max = currWordScore.value
+                }
+            }
+        }
+        //console.log(max);
+        for (let i = 0; i < docs.length; i++)  {
+            for (let j = 0; j < docs[i].wordScores.length; j++)  {
+                docs[i].wordScores[j].valueNormalized = docs[i].wordScores[j].value / max;
+            }
+        }
+        console.log(docs);
         return docs;
     }
 
