6af0fda0a66c9451e7efb5755ffd9466fa73b03b
Rob Carney <robert.edward.carney@gmail.com>
2019-03-06 17:15:12 -0500

    Making cursor default on hover over splitters in DocResult

--- src/components/SplitterVizualization.js
+++ src/components/SplitterVizualization.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SplitterVisualization = ({content}) =>
-    <span>{content}</span>
+    <span className="splitter-result">{content}</span>
 
 export default SplitterVisualization;
 
