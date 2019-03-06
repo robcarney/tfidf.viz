84977392b180c1be42c1881fece44590d11c3baf
Rob Carney <robert.edward.carney@gmail.com>
2019-03-06 18:05:18 -0500

    Restyling sidebar

--- src/components/TfidfVizSidebar.js
+++ src/components/TfidfVizSidebar.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const TfidfVizSidebar = () =>
     <div className="col-3 sidebar no-float h-100">
-        <div className="">
+        <div className="sidebar-content">
             <h1>Tfidf.Viz</h1>
+            <p>A visualization tool for the TF-IDF algorithm.</p>
         </div>
     </div>;
 
