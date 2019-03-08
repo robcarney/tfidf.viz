8b1f395f955806c7aedad007482a4f56aa8c9dbe
Rob Carney <robert.edward.carney@gmail.com>
2019-03-08 13:35:54 -0500

    Added github fa link in sidebar brand

--- src/components/TfidfViz.js
+++ src/components/TfidfViz.js
@@ -3,7 +3,13 @@ import React, {Component} from 'react';
 import TfidfVizSidebar from './TfidfVizSidebar'
 import DocumentList from './DocumentList'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faGithubAlt, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+
 import '../style/TfidfViz.css';
+
+library.add(faGithub, faGithubAlt, faGithubSquare);
 
 class TfidfViz extends Component  {
     constructor(props)  {
--- src/components/TfidfVizSidebar.js
+++ src/components/TfidfVizSidebar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
+import {faGithubAlt, faGithubSquare} from "@fortawesome/free-brands-svg-icons";
+
 const TfidfVizSidebar = () =>
     <div className="col-3 sidebar no-float h-100">
         <div className="sidebar-content">
             <h1>Tfidf.Viz</h1>
             <p>A visualization tool for the TF-IDF algorithm.</p>
+            <a className="github-link" href="https://github.com/robcarney/tfidf.viz">
+                <FontAwesomeIcon size="2x" icon={faGithub}/>
+            </a>
         </div>
     </div>;
 
